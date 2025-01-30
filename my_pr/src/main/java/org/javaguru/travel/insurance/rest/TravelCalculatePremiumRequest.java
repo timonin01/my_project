c7fcdadb8344792e74744bc2b0668abbae432899
timonin01 <atimonin2006@gmail.com>
2025-01-30 20:23:44 +0300

    Добавил коструткоры, get/set в TravelCalculatePremiumRequest

--- my_pr/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ my_pr/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -9,4 +9,45 @@ public class TravelCalculatePremiumRequest {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
+    public TravelCalculatePremiumRequest() {
+    }
+
+    public TravelCalculatePremiumRequest(String personFirstName, String personLastName, Date agreementDateFrom, Date agreementDateTo) {
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.agreementDateFrom = agreementDateFrom;
+        this.agreementDateTo = agreementDateTo;
+    }
+
+    public String getPersonFirstName() {
+        return personFirstName;
+    }
+
+    public void setPersonFirstName(String personFirstName) {
+        this.personFirstName = personFirstName;
+    }
+
+    public String getPersonLastName() {
+        return personLastName;
+    }
+
+    public void setPersonLastName(String personLastName) {
+        this.personLastName = personLastName;
+    }
+
+    public Date getAgreementDateFrom() {
+        return agreementDateFrom;
+    }
+
+    public void setAgreementDateFrom(Date agreementDateFrom) {
+        this.agreementDateFrom = agreementDateFrom;
+    }
+
+    public Date getAgreementDateTo() {
+        return agreementDateTo;
+    }
+
+    public void setAgreementDateTo(Date agreementDateTo) {
+        this.agreementDateTo = agreementDateTo;
+    }
 }
