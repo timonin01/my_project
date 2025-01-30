8f344e9a2fb7f0af9964165208eacc704771d19c
timonin01 <atimonin2006@gmail.com>
2025-01-30 16:30:51 +0300

    Create folder

--- student_khamit_bilyaletdinov/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
+++
@@ -1,8 +0,0 @@
-package org.javaguru.travel.insurance.core;
-
-import java.math.BigDecimal;
-import java.util.Date;
-
-public interface DateTimeService {
-    public BigDecimal calculateAgreementPrice(Date startDate, Date endDate);
-}
