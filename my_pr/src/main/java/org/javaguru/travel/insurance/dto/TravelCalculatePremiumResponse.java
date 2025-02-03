aae5a0abfbf66f0167f015cc4198b5d4f959d65b
timonin01 <atimonin2006@gmail.com>
2025-02-03 18:36:09 +0300

    add jsonFormat to agreementDateFrom and agreementDateTo

--- my_pr/src/main/java/org/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ my_pr/src/main/java/org/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -1,5 +1,6 @@
 package org.javaguru.travel.insurance.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,11 @@ public class TravelCalculatePremiumResponse extends CoreResponse {
 
     private String personFirstName;
     private String personLastName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateFrom;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
 
