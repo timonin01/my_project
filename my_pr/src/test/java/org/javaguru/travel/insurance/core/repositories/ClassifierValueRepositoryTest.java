d7ce06de948b545849b0fcd05bac300a3fa5f24b
timonin01 <atimonin2006@gmail.com>
2025-02-10 19:30:32 +0300

    change ClassifierValueRepositoryTest with @ParameterizedTest

--- my_pr/src/test/java/org/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
+++ my_pr/src/test/java/org/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
@@ -4,10 +4,14 @@ import org.javaguru.travel.insurance.core.domain.ClassifierValue;
 import org.javaguru.travel.insurance.core.repositories.ClassifierValueRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,53 +28,29 @@ class ClassifierValueRepositoryTest {
         assertNotNull(classifierValueRepository);
     }
 
-    @Test
-    public void shouldFind_RiskType_TRAVEL_MEDICAL() {
-        shouldFind_RiskType("RISK_TYPE", "TRAVEL_MEDICAL");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_CANCELLATION() {
-        shouldFind_RiskType("RISK_TYPE", "TRAVEL_CANCELLATION");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_LOSS_BAGGAGE() {
-        shouldFind_RiskType("RISK_TYPE", "TRAVEL_LOSS_BAGGAGE");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_THIRD_PARTY_LIABILITY() {
-        shouldFind_RiskType("RISK_TYPE", "TRAVEL_THIRD_PARTY_LIABILITY");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_EVACUATION() {
-        shouldFind_RiskType("RISK_TYPE", "TRAVEL_EVACUATION");
-    }
-
-    @Test
-    public void shouldFind_RiskType_TRAVEL_SPORT_ACTIVITIES() {
-        shouldFind_RiskType("RISK_TYPE", "TRAVEL_SPORT_ACTIVITIES");
-    }
-
-    @Test
-    public void shouldNotFind_RiskType_FAKE() {
-        shouldNotFind_RiskType("RISK_TYPE", "FAKE");
-    }
-
-    public void shouldFind_RiskType(String classifierTitle, String ic){
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                classifierTitle, ic);
+    @ParameterizedTest
+    @MethodSource("riskTypeIc")
+    public void shouldFindRiskTypeValueByIc(String ic) {
+        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc("RISK_TYPE", ic);
         assertTrue(valueOpt.isPresent());
+        assertEquals(valueOpt.get().getClassifier().getTitle(), "RISK_TYPE");
         assertEquals(valueOpt.get().getIc(), ic);
-        assertEquals(valueOpt.get().getClassifier().getTitle(), classifierTitle);
     }
 
-    public void shouldNotFind_RiskType(String classifierTitle, String ic){
-        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc(
-                classifierTitle,ic);
+    private static List<String> riskTypeIc() {
+        return List.of(
+                "TRAVEL_MEDICAL",
+                "TRAVEL_CANCELLATION",
+                "TRAVEL_LOSS_BAGGAGE",
+                "TRAVEL_THIRD_PARTY_LIABILITY",
+                "TRAVEL_EVACUATION",
+                "TRAVEL_SPORT_ACTIVITIES"
+        );
+    }
+
+    @Test
+    public void shouldNotFind_RiskType() {
+        Optional<ClassifierValue> valueOpt = classifierValueRepository.findByClassifierTitleAndIc("RISK_TYPE", "FAKE");
         assertTrue(valueOpt.isEmpty());
     }
-
 }
