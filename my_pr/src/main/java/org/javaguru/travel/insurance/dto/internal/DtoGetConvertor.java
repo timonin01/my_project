77e64d4768da0d3ce938417c88dc4f0da78f5fd8
timonin01 <atimonin2006@gmail.com>
2025-03-04 16:25:34 +0300

    connect REST and getAgreement

---
+++ my_pr/src/main/java/org/javaguru/travel/insurance/dto/internal/DtoGetConvertor.java
@@ -0,0 +1,86 @@
+package org.javaguru.travel.insurance.dto.internal;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.javaguru.travel.insurance.core.api.command.TravelCalculatePremiumCoreCommand;
+import org.javaguru.travel.insurance.core.api.command.TravelGetAgreementCoreCommand;
+import org.javaguru.travel.insurance.core.api.command.TravelGetAgreementCoreResult;
+import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
+import org.javaguru.travel.insurance.core.api.dto.RiskDTO;
+import org.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
+import org.javaguru.travel.insurance.dto.RiskPremium;
+import org.javaguru.travel.insurance.dto.ValidationError;
+import org.javaguru.travel.insurance.dto.v2.PersonResponseDTO;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DtoGetConvertor {
+
+    public TravelGetAgreementCoreCommand buildCoreComand(String uuid){
+        return new TravelGetAgreementCoreCommand(uuid);
+    }
+    public TravelGetAgreementResponse buildResponse(TravelGetAgreementCoreResult coreResult){
+        return (coreResult.hasErrors())
+                ? buildResponseWithErrors(coreResult.getErrors())
+                : buildSuccessfulResponse(coreResult);
+    }
+
+    private TravelGetAgreementResponse buildResponseWithErrors(List<ValidationErrorDTO> errors){
+        List<ValidationError> error = transformValidationErrorsToResponse(errors);
+        return new TravelGetAgreementResponse(error);
+    }
+
+    private List<ValidationError> transformValidationErrorsToResponse(List<ValidationErrorDTO> errors){
+        return errors.stream().
+                map(error -> new ValidationError(error.getErrorCode(), error.getDescription()))
+                .collect(Collectors.toList());
+    }
+
+    private TravelGetAgreementResponse buildSuccessfulResponse(TravelGetAgreementCoreResult coreResult){
+        TravelGetAgreementResponse response = new TravelGetAgreementResponse();
+        AgreementDTO agreement = coreResult.getAgreement();
+        loadGetResponseFields(agreement,response);
+        loadGetPersons(agreement,response);
+        return response;
+    }
+
+    private void loadGetResponseFields(AgreementDTO agreement, TravelGetAgreementResponse response){
+        response.setUuid(agreement.getUuid());
+        response.setCountry(agreement.getCountry());
+        response.setAgreementDateTo(agreement.getAgreementDateTo());
+        response.setAgreementDateFrom(agreement.getAgreementDateFrom());
+        response.setAgreementPremium(agreement.getAgreementPremium());
+    }
+
+    private void loadGetPersons(AgreementDTO agreement, TravelGetAgreementResponse response){
+        List<PersonResponseDTO> personResponseDTOS = agreement.getPersons().stream().
+                map(this::buildPersonFromResponse).
+                toList();
+    }
+
+    private PersonResponseDTO buildPersonFromResponse(PersonDTO personDTO){
+        PersonResponseDTO person = new PersonResponseDTO();
+        person.setPersonFirstName(personDTO.getPersonFirstName());
+        person.setPersonLastName(personDTO.getPersonLastName());
+        person.setPersonCode(personDTO.getPersonCode());
+        person.setPersonBirthDate(personDTO.getPersonBirthDate());
+        person.setMedicalRiskLimitLevel(personDTO.getMedicalRiskLimitLevel());
+
+        person.setPersonPremium(personDTO.getRisks().stream()
+                .map(RiskDTO::getPremium)
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+        );
+
+        person.setPersonRisks(personDTO.getRisks().stream()
+                .map(riskDTO -> new RiskPremium(riskDTO.getRiskIc(), riskDTO.getPremium()))
+                .collect(Collectors.toList()));
+
+        return person;
+    }
+
+}
--- my_pr/src/main/java/org/javaguru/travel/insurance/rest/internal/TravelGetAgreementRestController.java
+++ my_pr/src/main/java/org/javaguru/travel/insurance/rest/internal/TravelGetAgreementRestController.java
@@ -3,12 +3,14 @@ package org.javaguru.travel.insurance.rest.internal;
 import com.google.common.base.Stopwatch;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.javaguru.travel.insurance.core.api.command.TravelCalculatePremiumCoreCommand;
+import org.javaguru.travel.insurance.core.api.command.TravelGetAgreementCoreCommand;
+import org.javaguru.travel.insurance.core.api.command.TravelGetAgreementCoreResult;
+import org.javaguru.travel.insurance.core.services.TravelGetAgreementService;
+import org.javaguru.travel.insurance.dto.internal.DtoGetConvertor;
 import org.javaguru.travel.insurance.dto.internal.TravelGetAgreementResponse;
 import org.javaguru.travel.insurance.rest.common.TravelCalculatePremiumRequestExecutionTimeLogger;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -20,27 +22,25 @@ public class TravelGetAgreementRestController {
     private final TravelGetAgreementRequestLogger requestLogger;
     private final TravelGetAgreementResponseLogger responseLogger;
     private final TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
+    private final TravelGetAgreementService service;
+    private final DtoGetConvertor dtoGetConvertor;
 
     @GetMapping(path = "/{uuid}",
             produces = "application/json")
     public TravelGetAgreementResponse getAgreement(@PathVariable("uuid") String uuid) {
         Stopwatch stopwatch = Stopwatch.createStarted();
         TravelGetAgreementResponse response = processRequest(uuid);
-        new TravelGetAgreementResponse();
-        response.setAgreementDateFrom(new Date());
-        response.setAgreementDateTo(new Date());
-        response.setUuid(uuid);
         executionTimeLogger.timeRecordertoLogger(stopwatch);
         return response;
     }
 
+
     private TravelGetAgreementResponse processRequest(String uuid) {
         requestLogger.log(uuid);
 
-        TravelGetAgreementResponse response = new TravelGetAgreementResponse();
-        response.setAgreementDateFrom(new Date());
-        response.setAgreementDateTo(new Date());
-        response.setUuid(uuid);
+        TravelGetAgreementCoreCommand coreCommand = dtoGetConvertor.buildCoreComand(uuid);
+        TravelGetAgreementCoreResult coreResult = service.getAgreement(coreCommand);
+        TravelGetAgreementResponse response = dtoGetConvertor.buildResponse(coreResult);
 
         responseLogger.log(response);
         return response;
