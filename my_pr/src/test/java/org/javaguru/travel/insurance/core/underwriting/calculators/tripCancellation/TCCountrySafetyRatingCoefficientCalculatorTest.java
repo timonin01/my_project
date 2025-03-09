6534dcac2dfd6738f7bada29af1ab94531e39965
timonin01 <atimonin2006@gmail.com>
2025-03-09 15:36:26 +0300

    write test for TravelCancellationRiskCalculator

--- my_pr/src/test/java/org/javaguru/travel/insurance/core/underwriting/calculators/tripCancellation/TCCountrySafetyRatingCoefficientCalculatorTest.java
+++ my_pr/src/test/java/org/javaguru/travel/insurance/core/underwriting/calculators/tripCancellation/TCCountrySafetyRatingCoefficientCalculatorTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class TCCountryDefaultDayRateCalculatorTest {
+class TCCountrySafetyRatingCoefficientCalculatorTest {
 
     @Mock private TCCountrySafetyRatingCoefficientRepository countrySafetyRatingCoefficientRepository;
 
