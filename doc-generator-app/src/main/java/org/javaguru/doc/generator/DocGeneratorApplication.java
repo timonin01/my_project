38ed09c7c669aa54c33eff8effe22f5c2fd316a7
timonin01 <atimonin2006@gmail.com>
2025-04-06 13:07:25 +0300

    add new directory - doc-generator-app

---
+++ doc-generator-app/src/main/java/org/javaguru/doc/generator/DocGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.javaguru.doc.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocGeneratorApplication.class, args);
+    }
+
+}
